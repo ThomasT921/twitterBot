a65b7f4ca29b5cd3e3378f09bbe6d1d718e676ee
Thomas Taylor <Thomastaylor921@gmail.com>
2020-10-03 10:51:24 -0400

    Changed API's, removed from bot, made config file and hid it

--- mybot.py
+++ mybot.py
@@ -1,11 +1,6 @@
 
-
 import tweepy
-
-consumer_key = "QuNadPar8VBpuYw3XJlmYQZkC"
-consumer_secret = "BdfkGlw0pcYTfOI2p5KM7k6KxSXlcvlnejVRmGEa58FBc4PAvi"
-access_token = "949582798700142592-mb1LNMnL4WZh4vOCxaAeC1abwBbHEBs"
-access_token_secret = "JSeFit2now612KsQGGdRTbXt9Cn19WwFlmOpykiax63mN"
+from config import *
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
